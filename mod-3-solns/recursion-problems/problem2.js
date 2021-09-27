e61e8d982d6e06c8a19ef8a578fb52a06418aa9a
Tolulope Oyeniyi <toyeniyi@Tolulopes-MacBook-Pro.local>
2021-09-27 08:40:37 -0500

    Update recursion problems.

--- mod-3-solns/recursion-problems/problem2.js
+++ mod-3-solns/recursion-problems/problem2.js
@@ -1,12 +1,12 @@
 // Problem 2: Write a JavaScript program to compute the sum of an array of integers. 
 
-function sum_array(my_array) {
-  if (my_array.length === 1) {
-    return my_array[0];
+function sumArray(arr) {
+  if (arr.length === 1) {
+    return arr[0];
   }
   else {
-    return my_array.pop() + sum_array(my_array);
+    return arr.pop() + sumArray(arr);
   }
 };
 
-console.log(sum_array([1,2,3,4,5,6]));
+console.log(sumArray([1,2,3,4,5,6]));
