c48964635dbe086d8406bd33ecaebc2a37378934
Tolu (Tj) Oyeniyi <16782808+tjoye20@users.noreply.github.com>
2022-03-04 00:36:59 -0600

    Greatest to least.

--- mod-3-solns/array-problems/problem3.js
+++ mod-3-solns/array-problems/problem3.js
@@ -8,12 +8,13 @@ Example array: var arr1=[-3,8,7,6,5,-4,3,2,1];
 const arr1 = [-3,8,7,6,5,-4,3,2,1,23,16,66,178,235,5677,-14123,0,-44,-21];
 
 function sortNumber(arr){
-    return arr.sort(function(a,b){ 
+    return arr.sort(function(a,b){
+        // look at the return values
         if (a < b) {
-            return -1;
+            return 1;
         }
         if(a > b) {
-            return 1;
+            return -1;
         }
         return 0;
     });
